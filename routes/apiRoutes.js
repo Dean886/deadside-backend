afcd73eb069ec1d86fd0c0bd22d60dc3e312febb
Dean886 <deenbzubi@gmail.com>
2025-05-19 22:32:11 -0500

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,6 +4,7 @@ const { login } = require("../controllers/authController");
 const verifyToken = require("../middleware/authMiddleware");
 
 router.post("/login", login);
+
 router.get("/protected", verifyToken, (req, res) => {
   res.json({ message: `🔐 Authorized access granted`, user: req.user });
 });
